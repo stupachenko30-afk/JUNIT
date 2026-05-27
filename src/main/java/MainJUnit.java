2ae480f330e61adf10ee339b4d14918b33648795
Yevhenii <stupachenko30@gmail.com>
2026-05-27 11:35:18 +0200

    add

---
+++ src/main/java/MainJUnit.java
@@ -0,0 +1,21 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MainJUnit {
+    public static void main(String[] args) {
+      int a = 5, b = 5;
+      assertTrue(a==b);
+      assertFalse(a==b);
+        System.out.println("the end");
+
+    }
+}
+
+
+/*
+  int a = 5, b = 3;
+        assertFalse  (a == b);
+
+
+        System.out.println("the end");
+
+ */
