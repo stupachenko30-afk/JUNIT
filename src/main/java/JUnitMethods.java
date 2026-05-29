8881bad5ade85717787f6f3ef35b12f3c7c3c71d
Yevhenii <stupachenko30@gmail.com>
2026-05-29 10:53:15 +0200

    add

--- src/main/java/JUnitMethods.java
+++ src/main/java/JUnitMethods.java
@@ -7,20 +7,122 @@ public class JUnitMethods {
 
     @Test
     public void concatStringArray(){
-        String[] ar = {"hello", "bye", "money", "happy", "life"};
+        String[] ar = {"hello", "", "money", "happy", " ", null};
         String res = MethodsForTest.concatStringArray(ar,0,2);
-        assertEquals("hellomaney",res);
+        assertEquals("hellomoney",res);
+
+        res = MethodsForTest.concatStringArray(ar,0,0);
+        assertEquals("hellohello",res);
+
+        res = MethodsForTest.concatStringArray(ar,1,2);
+        assertEquals("money",res);
+
+        res = MethodsForTest.concatStringArray(ar,3,4);
+        assertEquals("happy ",res);
+
+
+
+
+
+        res = MethodsForTest.concatStringArray(null,1,6);
+        assertNull(res);
+
+
+        String[]ar1={};
+        res = MethodsForTest.concatStringArray(ar1,1, 2);
+        assertNull(res);
+        String[]ar2= {"\0", "hello", "life"};
+        res = MethodsForTest.concatStringArray(ar2,1, 2);
+        assertEquals("hellolife", res);
+        res = MethodsForTest.concatStringArray(ar,-1, 2);
+        assertNull(res);
+        res = MethodsForTest.concatStringArray(ar,1, 5);
+        assertNull(res);
+        res = MethodsForTest.concatStringArray(ar,1, -2);
+        assertNull(res);
+
+
 
 
     }
 
         @Test
         public void sumElementsArray() {
+        //           0  1  2   3    4  5
+        int[] ar = {-1, 9, 0, 24, -56, 0, Integer.MAX_VALUE };
+//positive
+Integer res = MethodsForTest.sumElementsArray(ar,0,4);
+assertTrue(-57 == res);
+
+            res = MethodsForTest.sumElementsArray(ar,0,3);
+            assertTrue(23 == res);
+            res = MethodsForTest.sumElementsArray(ar,1,3);
+            assertTrue(33 == res);
+            res = MethodsForTest.sumElementsArray(ar,2,5);
+            assertTrue(0 == res);
+            res = MethodsForTest.sumElementsArray(ar,3,1);
+            assertTrue(33 == res);
+            res = MethodsForTest.sumElementsArray(ar,1,1);
+            assertTrue(18 == res);
+            res = MethodsForTest.sumElementsArray(ar,4,4);
+            assertTrue(-112 == res);
+            res = MethodsForTest.sumElementsArray(ar,2,3);
+            assertTrue(24 == res);
+            res = MethodsForTest.sumElementsArray(ar,4,5);
+            assertTrue(-56 == res);
 
 
-
-
+            //negative
+            res = MethodsForTest.sumElementsArray(null,4,5);
+            assertNull(res);
+            int[] ar1 = {};
+            res = MethodsForTest.sumElementsArray(ar1,4,5);
+            assertNull(res);
+            res = MethodsForTest.sumElementsArray(ar,0,9);
+            assertNull(res);
+            res = MethodsForTest.sumElementsArray(ar,-1,5);
+            assertNull(res);
+            res = MethodsForTest.sumElementsArray(ar,4,-1);
+            assertNull(res);
+            res = MethodsForTest.sumElementsArray(ar,0,9);
+            assertNull(res);
+            res = MethodsForTest.sumElementsArray(ar,9,5);
+            assertNull(res);
+            res = MethodsForTest.sumElementsArray(ar,ar.length,9);
+            assertNull(res);
+            res = MethodsForTest.sumElementsArray(ar,4,ar.length);
+            assertNull(res);
+            res = MethodsForTest.sumElementsArray(ar,6,1);
+            assertNull(res);
 
     }
+/*
+1.
+puplic static String concatWithSym(String s1,String s2, char sym)
+s1-> "hello"
+s2-> "money"
+sym '#'
+concat and return "hello#money"
+in case any problem return null
 
+2.
+ public static String concatStrs (String[] ar1, String [] ar2,
+                    0     1     2        3              int index1,int index2)
+         ar1->["hello"]["bye"]["money"]["happy"]
+         index1 = 2
+                0      1       2
+         ar2->["yes"]["no"]["maybe"]
+         index2 = 1
+
+        concat and return "moneyno"
+        in case any problem return null
+
+
+
+Write JUnit test TDD
+
+
+
+
+ */
 }
--- src/main/java/MethodsForTest.java
+++ src/main/java/MethodsForTest.java
@@ -1,54 +1,71 @@
 import org.junit.jupiter.api.Test;
-
-/*
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-
-public class MethodsForTest {
-    public static int parseAndSum (String s1, String s2){
-
-    }
-}
-
-
-
-
-@Test
-void testparseAndSum(){
-    String res = Integer.parseInt(12,3);
-    assertEquals(15,);
-}*/
 public class MethodsForTest {
     //TDD -> test driven development
     public static String concatStringArray(
             String[] strArray,
             int indexFirst,
             int indexSecond){
+        //Проверка всего массива на null
+//        for(String s: strArray){
+//            if (s == null){
+//                return null;
+//            }
+    //    }
+
+        if(strArray == null || strArray.length==0
+                || indexFirst<0 || indexSecond<0
+                || indexFirst >= strArray.length
+                || indexSecond >= strArray.length
+                || strArray[indexFirst] == null
+                || strArray[indexSecond] == null
+        )
         return null;
+        return strArray[indexFirst]+strArray[indexSecond];
     }
 
-    /*
+   /*
                    0       1       2        3        4
     strArray-> ["Hello"]["bye"]["Money"]["happy"][" life"]
     indexFirst = 1   indexSecond = 3
     return "byehappy"
+*/
 
 
-     */
 
 
 
 
     public static Integer sumElementsArray(
     int[] array,
-    int indexFirst,
-    int indexSecond){
-        return null;
+      int indexFirst,
+        int indexSecond) {
+
+
+        if (array == null || array.length == 0
+                || indexFirst < 0 || indexSecond < 0
+                || indexFirst >= array.length
+                || indexSecond >= array.length)
+
+            return null;
+
+        if (array[indexFirst] == Integer.MAX_VALUE && array[indexSecond] > 0)
+            return null;
+        if (array[indexSecond] == Integer.MAX_VALUE && array[indexFirst] > 0)
+            return null;
+        if (array[indexFirst] == Integer.MIN_VALUE && array[indexSecond] < 0)
+            return null;
+        if (array[indexSecond] == Integer.MIN_VALUE && array[indexFirst] < 0)
+            return null;
+        return array[indexFirst] +array[indexSecond];
+    }
+
 }
 
 
-}
+
+
 /*
            0  1  2  4  4  5       indexFirst = 2   indexSecond = 4
 array ->  [9][2][7][1][4][8]
